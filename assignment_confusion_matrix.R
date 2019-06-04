a4db1ca626b9b830b513285027377442d97e91fb
jahoey <jahoey@users.noreply.github.com>
2019-06-04 17:14:56 -0400

    expand assignments for GAM pops 4-10 & plot confusion matrix

--- assignment_confusion_matrix.R
+++ assignment_confusion_matrix.R
@@ -68,14 +68,14 @@ for (i in 1:ncol(adult10.freqs[10,])){
 # Population likelihoods for 10 groups
 pop1.likelihoods <- array(dim = c(1000,10,10))
 pop2.likelihoods <- array(dim = c(1000,10,10))
-# pop3.likelihoods <- array(dim = c(1000,10,10))
-# pop4.likelihoods <- array(dim = c(1000,10,10))
-# pop5.likelihoods <- array(dim = c(1000,10,10))
-# pop6.likelihoods <- array(dim = c(1000,10,10))
-# pop7.likelihoods <- array(dim = c(1000,10,10))
-# pop8.likelihoods <- array(dim = c(1000,10,10))
-# pop9.likelihoods <- array(dim = c(1000,10,10))
-# pop10.likelihoods <- array(dim = c(1000,10,10))
+pop3.likelihoods <- array(dim = c(1000,10,10))
+pop4.likelihoods <- array(dim = c(1000,10,10))
+pop5.likelihoods <- array(dim = c(1000,10,10))
+pop6.likelihoods <- array(dim = c(1000,10,10))
+pop7.likelihoods <- array(dim = c(1000,10,10))
+pop8.likelihoods <- array(dim = c(1000,10,10))
+pop9.likelihoods <- array(dim = c(1000,10,10))
+pop10.likelihoods <- array(dim = c(1000,10,10))
 
 # For larvae simulated using GAM pop 1 allele freqs. Then geno likelihoods calculated for all 10 GAM pops
 for (h in 1:nrow(adult10.freqs)){
@@ -134,44 +134,303 @@ for (h in 1:nrow(adult10.freqs)){
   }
 }
 
+# GAM pop 4
+for (h in 1:nrow(adult10.freqs)){
+  
+  for (j in 1:nrow(Pop4.alleles)){
+    
+    for (i in 1:ncol(Pop4.alleles)){
+      if(Pop4.alleles[j,i] == 2) {
+        pop4.likelihoods[j,i,h] <- adult10.freqs[h,i]^2
+      } else if (Pop4.alleles[j,i] == 1) {
+        pop4.likelihoods[j,i,h] <- 2*(adult10.freqs[h,i] * (1-adult10.freqs[h,i]))
+      } else if (Pop4.alleles[j,i] == 0) {
+        pop4.likelihoods[j,i,h] <- ( 1-adult10.freqs[h,i])^2 
+      } else {
+        pop4.likelihoods[j,i,h] <- 1
+      }
+    }
+  }
+}
 
 
+# GAM pop 5
+for (h in 1:nrow(adult10.freqs)){
+  
+  for (j in 1:nrow(Pop5.alleles)){
+    
+    for (i in 1:ncol(Pop5.alleles)){
+      if(Pop5.alleles[j,i] == 2) {
+        pop5.likelihoods[j,i,h] <- adult10.freqs[h,i]^2
+      } else if (Pop5.alleles[j,i] == 1) {
+        pop5.likelihoods[j,i,h] <- 2*(adult10.freqs[h,i] * (1-adult10.freqs[h,i]))
+      } else if (Pop5.alleles[j,i] == 0) {
+        pop5.likelihoods[j,i,h] <- ( 1-adult10.freqs[h,i])^2 
+      } else {
+        pop5.likelihoods[j,i,h] <- 1
+      }
+    }
+  }
+}
+
+# GAM pop 6
+for (h in 1:nrow(adult10.freqs)){
+  
+  for (j in 1:nrow(Pop6.alleles)){
+    
+    for (i in 1:ncol(Pop6.alleles)){
+      if(Pop6.alleles[j,i] == 2) {
+        pop6.likelihoods[j,i,h] <- adult10.freqs[h,i]^2
+      } else if (Pop6.alleles[j,i] == 1) {
+        pop6.likelihoods[j,i,h] <- 2*(adult10.freqs[h,i] * (1-adult10.freqs[h,i]))
+      } else if (Pop6.alleles[j,i] == 0) {
+        pop6.likelihoods[j,i,h] <- ( 1-adult10.freqs[h,i])^2 
+      } else {
+        pop6.likelihoods[j,i,h] <- 1
+      }
+    }
+  }
+}
+
+# GAM pop 7
+for (h in 1:nrow(adult10.freqs)){
+  
+  for (j in 1:nrow(Pop7.alleles)){
+    
+    for (i in 1:ncol(Pop7.alleles)){
+      if(Pop7.alleles[j,i] == 2) {
+        pop7.likelihoods[j,i,h] <- adult10.freqs[h,i]^2
+      } else if (Pop7.alleles[j,i] == 1) {
+        pop7.likelihoods[j,i,h] <- 2*(adult10.freqs[h,i] * (1-adult10.freqs[h,i]))
+      } else if (Pop7.alleles[j,i] == 0) {
+        pop7.likelihoods[j,i,h] <- ( 1-adult10.freqs[h,i])^2 
+      } else {
+        pop7.likelihoods[j,i,h] <- 1
+      }
+    }
+  }
+}
+
+# GAM pop 8
+for (h in 1:nrow(adult10.freqs)){
+  
+  for (j in 1:nrow(Pop8.alleles)){
+    
+    for (i in 1:ncol(Pop8.alleles)){
+      if(Pop8.alleles[j,i] == 2) {
+        pop8.likelihoods[j,i,h] <- adult10.freqs[h,i]^2
+      } else if (Pop8.alleles[j,i] == 1) {
+        pop8.likelihoods[j,i,h] <- 2*(adult10.freqs[h,i] * (1-adult10.freqs[h,i]))
+      } else if (Pop8.alleles[j,i] == 0) {
+        pop8.likelihoods[j,i,h] <- ( 1-adult10.freqs[h,i])^2 
+      } else {
+        pop8.likelihoods[j,i,h] <- 1
+      }
+    }
+  }
+}
+
+# GAM pop 9
+for (h in 1:nrow(adult10.freqs)){
+  
+  for (j in 1:nrow(Pop9.alleles)){
+    
+    for (i in 1:ncol(Pop9.alleles)){
+      if(Pop9.alleles[j,i] == 2) {
+        pop9.likelihoods[j,i,h] <- adult10.freqs[h,i]^2
+      } else if (Pop9.alleles[j,i] == 1) {
+        pop9.likelihoods[j,i,h] <- 2*(adult10.freqs[h,i] * (1-adult10.freqs[h,i]))
+      } else if (Pop9.alleles[j,i] == 0) {
+        pop9.likelihoods[j,i,h] <- ( 1-adult10.freqs[h,i])^2 
+      } else {
+        pop9.likelihoods[j,i,h] <- 1
+      }
+    }
+  }
+}
+
+# GAM pop 10 is northern-most
+for (h in 1:nrow(adult10.freqs)){
+  
+  for (j in 1:nrow(Pop10.alleles)){
+    
+    for (i in 1:ncol(Pop10.alleles)){
+      if(Pop10.alleles[j,i] == 2) {
+        pop10.likelihoods[j,i,h] <- adult10.freqs[h,i]^2
+      } else if (Pop10.alleles[j,i] == 1) {
+        pop10.likelihoods[j,i,h] <- 2*(adult10.freqs[h,i] * (1-adult10.freqs[h,i]))
+      } else if (Pop10.alleles[j,i] == 0) {
+        pop10.likelihoods[j,i,h] <- ( 1-adult10.freqs[h,i])^2 
+      } else {
+        pop10.likelihoods[j,i,h] <- 1
+      }
+    }
+  }
+}
 
 
 #### Multiply rows together and then assign each individual to a GAM population ####
+# Pop 1
 Pop1.ass <- list()
 for (l in 1:10){
 Pop1.ass[[l]] <- apply(pop1.likelihoods[,,l], FUN = prod, MARGIN = 1, na.rm = TRUE)
   }
 
 Pop1.ass.df <- data.frame(matrix(unlist(Pop1.ass), nrow=nrow(pop1.likelihoods), byrow=F))
-colnames(Pop1.ass.df) <- c('Pop1', 'Pop2', 'Pop3', 'Pop4', 'Pop5', 'Pop6', 'Pop7', 'Pop8', 'Pop9', 'Pop10')
+colnames(Pop1.ass.df) <- c('Pop J', 'Pop I', 'Pop H', 'Pop G', 'Pop F', 'Pop E', 'Pop D', 'Pop C', 'Pop B', 'Pop A')
 Pop1.most.like <- colnames(Pop1.ass.df)[apply(Pop1.ass.df,1, which.max)]
 
+# Pop 2
 Pop2.ass <- list()
 for (l in 1:10){
   Pop2.ass[[l]] <- apply(pop2.likelihoods[,,l], FUN = prod, MARGIN = 1, na.rm = TRUE)
 }
   
 Pop2.ass.df <- data.frame(matrix(unlist(Pop2.ass), nrow=nrow(pop2.likelihoods), byrow=F))
-colnames(Pop2.ass.df) <- c('Pop1', 'Pop2', 'Pop3', 'Pop4', 'Pop5', 'Pop6', 'Pop7', 'Pop8', 'Pop9', 'Pop10')
+colnames(Pop2.ass.df) <- c('Pop J', 'Pop I', 'Pop H', 'Pop G', 'Pop F', 'Pop E', 'Pop D', 'Pop C', 'Pop B', 'Pop A')
 Pop2.most.like <- colnames(Pop2.ass.df)[apply(Pop2.ass.df,1, which.max)]
 
+# Pop 3
+Pop3.ass <- list()
+for (l in 1:10){
+  Pop3.ass[[l]] <- apply(pop3.likelihoods[,,l], FUN = prod, MARGIN = 1, na.rm = TRUE)
+}
 
+Pop3.ass.df <- data.frame(matrix(unlist(Pop3.ass), nrow=nrow(pop3.likelihoods), byrow=F))
+colnames(Pop3.ass.df) <- c('Pop J', 'Pop I', 'Pop H', 'Pop G', 'Pop F', 'Pop E', 'Pop D', 'Pop C', 'Pop B', 'Pop A')
+Pop3.most.like <- colnames(Pop3.ass.df)[apply(Pop3.ass.df,1, which.max)]
 
+# Pop 4
+Pop4.ass <- list()
+for (l in 1:10){
+  Pop4.ass[[l]] <- apply(pop4.likelihoods[,,l], FUN = prod, MARGIN = 1, na.rm = TRUE)
+}
 
+Pop4.ass.df <- data.frame(matrix(unlist(Pop4.ass), nrow=nrow(pop4.likelihoods), byrow=F))
+colnames(Pop4.ass.df) <- c('Pop J', 'Pop I', 'Pop H', 'Pop G', 'Pop F', 'Pop E', 'Pop D', 'Pop C', 'Pop B', 'Pop A')
+Pop4.most.like <- colnames(Pop4.ass.df)[apply(Pop4.ass.df,1, which.max)]
 
-Pop3.pop3ass <- apply(pop3.likelihoods, FUN = prod, MARGIN = 1, na.rm = TRUE)
-Pop4.pop4ass <- apply(pop4.likelihoods, FUN = prod, MARGIN = 1, na.rm = TRUE)
-Pop5.pop5ass <- apply(pop5.likelihoods, FUN = prod, MARGIN = 1, na.rm = TRUE)
-Pop6.pop6ass <- apply(pop6.likelihoods, FUN = prod, MARGIN = 1, na.rm = TRUE)
-Pop7.pop7ass <- apply(pop7.likelihoods, FUN = prod, MARGIN = 1, na.rm = TRUE)
-Pop8.pop8ass <- apply(pop8.likelihoods, FUN = prod, MARGIN = 1, na.rm = TRUE)
-Pop9.pop9ass <- apply(pop9.likelihoods, FUN = prod, MARGIN = 1, na.rm = TRUE)
-Pop10.pop10ass <- apply(pop10.likelihoods, FUN = prod, MARGIN = 1, na.rm = TRUE)
+# Pop 5
+Pop5.ass <- list()
+for (l in 1:10){
+  Pop5.ass[[l]] <- apply(pop5.likelihoods[,,l], FUN = prod, MARGIN = 1, na.rm = TRUE)
+}
+
+Pop5.ass.df <- data.frame(matrix(unlist(Pop5.ass), nrow=nrow(pop5.likelihoods), byrow=F))
+colnames(Pop5.ass.df) <- c('Pop J', 'Pop I', 'Pop H', 'Pop G', 'Pop F', 'Pop E', 'Pop D', 'Pop C', 'Pop B', 'Pop A')
+Pop5.most.like <- colnames(Pop5.ass.df)[apply(Pop5.ass.df,1, which.max)]
+
+# Pop 6
+Pop6.ass <- list()
+for (l in 1:10){
+  Pop6.ass[[l]] <- apply(pop6.likelihoods[,,l], FUN = prod, MARGIN = 1, na.rm = TRUE)
+}
+
+Pop6.ass.df <- data.frame(matrix(unlist(Pop6.ass), nrow=nrow(pop6.likelihoods), byrow=F))
+colnames(Pop6.ass.df) <- c('Pop J', 'Pop I', 'Pop H', 'Pop G', 'Pop F', 'Pop E', 'Pop D', 'Pop C', 'Pop B', 'Pop A')
+Pop6.most.like <- colnames(Pop6.ass.df)[apply(Pop6.ass.df,1, which.max)]
+
+# Pop 7
+Pop7.ass <- list()
+for (l in 1:10){
+  Pop7.ass[[l]] <- apply(pop7.likelihoods[,,l], FUN = prod, MARGIN = 1, na.rm = TRUE)
+}
+
+Pop7.ass.df <- data.frame(matrix(unlist(Pop7.ass), nrow=nrow(pop7.likelihoods), byrow=F))
+colnames(Pop7.ass.df) <- c('Pop J', 'Pop I', 'Pop H', 'Pop G', 'Pop F', 'Pop E', 'Pop D', 'Pop C', 'Pop B', 'Pop A')
+Pop7.most.like <- colnames(Pop7.ass.df)[apply(Pop7.ass.df,1, which.max)]
+
+# Pop 8
+Pop8.ass <- list()
+for (l in 1:10){
+  Pop8.ass[[l]] <- apply(pop8.likelihoods[,,l], FUN = prod, MARGIN = 1, na.rm = TRUE)
+}
+
+Pop8.ass.df <- data.frame(matrix(unlist(Pop8.ass), nrow=nrow(pop8.likelihoods), byrow=F))
+colnames(Pop8.ass.df) <- c('Pop J', 'Pop I', 'Pop H', 'Pop G', 'Pop F', 'Pop E', 'Pop D', 'Pop C', 'Pop B', 'Pop A')
+Pop8.most.like <- colnames(Pop8.ass.df)[apply(Pop8.ass.df,1, which.max)]
+
+# Pop 9
+Pop9.ass <- list()
+for (l in 1:10){
+  Pop9.ass[[l]] <- apply(pop9.likelihoods[,,l], FUN = prod, MARGIN = 1, na.rm = TRUE)
+}
+
+Pop9.ass.df <- data.frame(matrix(unlist(Pop9.ass), nrow=nrow(pop9.likelihoods), byrow=F))
+colnames(Pop9.ass.df) <- c('Pop J', 'Pop I', 'Pop H', 'Pop G', 'Pop F', 'Pop E', 'Pop D', 'Pop C', 'Pop B', 'Pop A')
+Pop9.most.like <- colnames(Pop9.ass.df)[apply(Pop9.ass.df,1, which.max)]
+
+# Pop 10
+Pop10.ass <- list()
+for (l in 1:10){
+  Pop10.ass[[l]] <- apply(pop10.likelihoods[,,l], FUN = prod, MARGIN = 1, na.rm = TRUE)
+}
+
+Pop10.ass.df <- data.frame(matrix(unlist(Pop10.ass), nrow=nrow(pop10.likelihoods), byrow=F))
+colnames(Pop10.ass.df) <- c('Pop J', 'Pop I', 'Pop H', 'Pop G', 'Pop F', 'Pop E', 'Pop D', 'Pop C', 'Pop B', 'Pop A')
+Pop10.most.like <- colnames(Pop10.ass.df)[apply(Pop10.ass.df,1, which.max)]
+
 
 #### Rbind & organize the assignment results for individuals simulated from each of 10 GAM populations ####
 # Row as reference? Column as predicted?
-table(Pop1.most.like)
+mat <- rbind(table(Pop10.most.like)/1000, table(Pop9.most.like)/1000, table(Pop8.most.like)/1000, table(Pop7.most.like)/1000, table(Pop6.most.like)/1000, table(Pop5.most.like)/1000, table(Pop4.most.like)/1000, table(Pop3.most.like)/1000, table(Pop2.most.like)/1000, table(Pop1.most.like)/1000) # Each row was drawn from a known population. This should be the reference. Columns are predicted.
+rownames(mat) <- c('Pop A', 'Pop B', 'Pop C', 'Pop D', 'Pop E', 'Pop F', 'Pop G', 'Pop H', 'Pop I', 'Pop J')
 
 #### Plot confusion matrix ####
+# image(1:ncol(mat), 1:nrow(mat), t(mat), col = rainbow(5), xlab = 'Predicted population', ylab = 'True population', axes = FALSE)
+# axis(1, 1:ncol(mat), colnames(mat))
+# axis(2, 1:nrow(mat), rownames(mat))
+
+# Nice plot
+png(file="~/Documents/Graduate School/Rutgers/Summer Flounder/Analysis/PADEconnectivity/assignment_confusion_matrix.png", width=7, height=5, res=300, units="in")
+
+par(
+  mar=c(5, 4, 4, 2), # panel magin size in "line number" units
+  mgp=c(3, 1, 0), # default is c(3,1,0); line number for axis label, tick label, axis
+  tcl=-0.5, # size of tick marks as distance INTO figure (negative means pointing outward)
+  cex=1, # character expansion factor; keep as 1; if you have a many-panel figure, they start changing the default!
+  ps=14 # point size, which is the font size
+)
+
+mat2 <- data.matrix(mat)
+# min <- min(mat, na.rm = TRUE)
+# max <- max(mat,na.rm = TRUE)
+
+rbPal <- colorRampPalette(c('blue', 'red'))
+nocol <- 6
+color <- rbPal(nocol)
+ColorLevels <- seq(0, 0.5, length=length(color)) # this makes the color bar nicer later on
+# ColorLevels <- seq(min, max, length=length(color))
+
+# Set layout.  We are going to include a colorbar next to plot.
+layout(matrix(data=c(1,2), nrow=1, ncol=2), widths=c(4,1),
+       heights=c(1,1))
+
+#plotting margins.  These seem to work well for me.
+par(mar = c(5,7,2.5,1), font = 2)
+
+# Plot it up!
+image(1:ncol(mat2), 1:nrow(mat2), t(mat2),
+      col=color, xlab="Predicted population", ylab="True population",
+      axes=FALSE, zlim = c(0, 0.5),
+      main= NA, xlim = c(0.5, 11), ylim = c(0.5, 11))
+# image(1:ncol(mat2), 1:nrow(mat2), t(mat2),
+#       col=color, xlab="Predicted population", ylab="True population",
+#       axes=FALSE, zlim = c(min, max),
+#       main= NA, xlim = c(0.5, 11), ylim = c(0.5, 11))
+abline(0,1)
+
+# Now annotate the plot
+axis(side = 1, at=seq(1,10,1), labels=LETTERS[1:10],
+     cex.axis=1.0)
+axis(side = 2, at=seq(1,10,1), labels=LETTERS[1:10], las= 1,
+     cex.axis=1)
+
+# Add colorbar to second plot region
+par(mar = c(3.5,2.5,3.5,2))
+image(1, ColorLevels,
+      matrix(data=ColorLevels, ncol=length(ColorLevels),nrow=1),
+      col=color,xlab="",ylab="",xaxt="n", las = 1)
+mtext('% Assignment', side = 3, cex = 0.7, line = 0.5, adj = 1)
+
+dev.off()
